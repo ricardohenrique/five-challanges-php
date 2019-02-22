d80019616201625e50bb334621bce258736338b6
ricardohenrique <ricardo.hb@outlook.com>
2019-02-22 12:11:34 -0300

    resolved thesaurus

--- Thesaurus/Thesaurus.php
+++ Thesaurus/Thesaurus.php
@@ -20,5 +20,21 @@ class Thesaurus
      */
     public function getSynonyms($word)
     {
+        $data['word'] = $word;
+        $data['synonyms'] = [];
+
+        if (array_key_exists($word, $this->thesaurus)) {
+            $data['synonyms'] = $this->thesaurus[$word];
+        }
+        return json_encode($data);
     }
 }
+
+// $givenThesaurus = new Thesaurus(
+//     [
+//         "buy" => ["purchase"],
+//         "big" => ["great", "large"],
+//     ]
+// );
+// $return = $givenThesaurus->getSynonyms('big');
+// var_dump($return);
