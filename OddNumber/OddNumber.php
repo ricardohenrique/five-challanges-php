3a08a5e89e249e40b0a366666909220d8b968a14
ricardohenrique <ricardo.hb@outlook.com>
2019-02-22 11:54:24 -0300

    resolve odd number

--- OddNumber/OddNumber.php
+++ OddNumber/OddNumber.php
@@ -21,5 +21,22 @@ class OddNumber
      */
     public function find()
     {
+        $countValues = array_count_values($this->numbers);
+        $countCount = array_count_values($countValues);
+
+        foreach ($countCount as $key => $value) {
+            if($value === 1) {
+                $get = array_search($key, $countValues);
+                return $get;
+            }
+        }
+
+        throw new Exception();
     }
 }
+
+// $odd = new OddNumber([2, 5, 9, 1, 5, 1, 8, 2, 8]);
+// $odd = new OddNumber([2, 3, 3, 1, 4, 5, 4, 1, 4, 2, 5]);
+// $return = $odd->find();
+
+// var_dump($return);
